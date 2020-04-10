bf8228ecfc1eee579639c1ccc2c301f33824fd7e
Alexander Filipin <af@ocg.de>
2020-04-10 20:24:09 +0200

    Cleaned misc script

--- Misc.ps1
+++ Misc.ps1
@@ -1,17 +1,13 @@
 # Connect to EXO 
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange `
 -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
--Credential $cred -Authentication Basic -AllowRedirection 
-# Gaaaa!! We are using basic auth for this.
- 
+-Credential $cred -Authentication Basic -AllowRedirection
+
 # Configure Mailbox CA Policy and remove the session
 Import-PSSession $Session -DisableNameChecking
 Set-OwaMailboxPolicy -Identity OwaMailboxPolicy-Default -ConditionalAccessPolicy ReadOnly
 Remove-PSSession $Session
- 
+
 # Connect to SPO and configure CA policy
-Connect-SPOService -Url https://M365x436188-admin.sharepoint.com -Credential $cred
+Connect-SPOService -Url https://tenant.sharepoint.com -Credential $cred
 Set-SPOTenant -ConditionalAccessPolicy AllowLimitedAccess
-# Note, this will create two CA policies that are enabled for all users!
-#>
-#Write-Host "Enabling app enforced restrictions not yet implemented, please do it manually"
